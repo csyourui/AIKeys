8de08bae9714f1ea6d4475b2721ddd78351e8e1f
csyourui <csyourui@gmail.com>
2025-04-01 10:52:44 +0800

    Initial Commit

---
+++ AIKeys/AIKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIKeysApp.swift
+//  AIKeys
+//
+//  Created by 尤瑞 on 2025/4/1.
+//
+
+import SwiftUI
+
+@main
+struct AIKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIKeysTests/AIKeysTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIKeysTests.swift
+//  AIKeysTests
+//
+//  Created by 尤瑞 on 2025/4/1.
+//
+
+import Testing
+@testable import AIKeys
+
+struct AIKeysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
